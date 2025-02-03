dadbb0e8373085cfc64b84073d7cd33f8cadfe2c
PandurangPailvan2502 <76768447+PandurangPailvan2502@users.noreply.github.com>
2025-02-03 09:28:14 +0000

    Copilot second commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.html file. Use the fs module to read the file and send it to the client.
+// The comments.html file should be in the same directory as the comments.js file.
+// The server should respond to GET requests to the /comments URL.
+// The server should respond with a 200 status code.
+// The server should respond with the contents of the comments.html file.
+// The server should respond with the correct Content-Type header.
+// The server should respond with the correct Content-Length header.
+// The server should respond with the correct Content-Encoding header.
+// The server should respond with the correct Transfer-Encoding header.
+// The server should respond with the correct Date header.
